168ea0bec65b5f2aa84038b4bff284a47e53e855
tanzeem <20bcs132@iiitdwd.ac.in>
2021-09-16 18:58:38 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+class content_management_system:
+    def __init__(self,who_created, description, file_size):
+        self.who_created = who_created
+        self.description = description
+        self.file_size = file_size
+
+PDFs = ('rahul', 'imp_file', '2mb')
+
+class content_management_system:
+   def __int__(self,who_created, description,image_dimensions,file_size,):
+        super().__init__(who_created, description, file_size)
+        self.image_dimensions = image_dimensions
+
+Pictures = ('satyam','personal_picture','1280 x 720 ', '4mb')
